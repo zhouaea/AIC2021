3bd5c4784ece124536ca417db987d14fbaf00a3c
Dylan <leddyd@bc.edu>
2021-07-24 14:28:40 -0400

    Pathfinding V.3 --> Major improvements to efficiency and stability.

--- ourplayer/Explorer.java
+++ ourplayer/Explorer.java
@@ -4,14 +4,17 @@ import aic2021.user.Location;
 import aic2021.user.UnitController;
 
 public class Explorer extends MyUnit {
+    Location testLocation = new Location(627, 406);
 
     Explorer(UnitController uc){
         super(uc);
     }
 
     void playRound(){
-        Location testLocation = new Location(313, 310);
+        lightTorch();
         goToLocation(testLocation);
+
+        FollowPath();
     }
 
 }
--- ourplayer/MyUnit.java
+++ ourplayer/MyUnit.java
@@ -4,32 +4,43 @@ import aic2021.user.Direction;
 import aic2021.user.Location;
 import aic2021.user.UnitController;
 import aic2021.user.UnitType;
+import aic2021.user.GameConstants;
+import java.lang.Math;
 
-import java.util.*;
-
-class FComparator implements Comparator<Waypoint> {
-    public int compare(Waypoint w1, Waypoint w2) {
-        if (w1.getFCost() == w2.getFCost()) {
-            return 0;
-        } else if (w1.getFCost() < w2.getFCost()) {
-            return -1;
-        } else {
-            return 1;
-        }
-    }
-}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
 class Waypoint {
     Location loc;
     int gCost, hCost;
     public Waypoint parent;
+    int heapIndex;
 
-    Waypoint(Location loc) {
+    public Waypoint(Location loc) {
         this.loc = loc;
     }
 
     int getFCost() {
         return gCost + hCost;
+    }
+
+    public boolean equals(Waypoint itemToCompare) {
+        return loc.isEqual(itemToCompare.loc);
+    }
+
+    public int compareTo(Waypoint itemToCompare) {
+        if (getFCost() < itemToCompare.getFCost()) {
+            return 1;
+        } else if (getFCost() == itemToCompare.getFCost()) {
+            if (hCost < itemToCompare.hCost) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else {
+            return -1;
+        }
     }
 }
 
@@ -86,23 +97,70 @@ public abstract class MyUnit {
     }
 
     //Pathfinder:
-    public PriorityQueue<Waypoint> ComputePath(Location dest) {
-        ArrayList<Waypoint> openList = new ArrayList<>();
-        ArrayList<Waypoint> closedList = new ArrayList<>();
-        PriorityQueue<Waypoint> waypoints = new PriorityQueue<>(new FComparator());
+    ArrayList<Waypoint> currentPath = new ArrayList<>();
+    Location currentDestination;
+
+    public boolean inClosedList(Waypoint item, HashSet<Waypoint> closedList) {
+        for (Waypoint w : closedList) {
+            if (w.equals(item)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * computeEstimate provides an alternative location if the original is
+     * undetectable. It arrives at coordinate by finding the intersections of
+     * the line connecting the unit and the destination and the unit's circle of
+     * detection.
+     * @param dest The destination the unit is expected to travel to.
+     * @return An alternative location.
+     */
+    public Location computeEstimate(Location dest) {
+        float x0, x1, y0, y1, r, m;
+        x0 = uc.getLocation().x;
+        y0 = uc.getLocation().y;
+        x1 = dest.x;
+        y1 = dest.y;
+        m = (y1 - y0) / (x1 - x0);
+        r = (16 - uc.getInfo().getDetectionLevel()) / 2;
+
+        double xCalc = r/Math.sqrt(1 + m * m);
+        double yCalc = r/Math.sqrt(1 + 1 / (m * m));
+
+        int xPos = (int)(x0 + xCalc);
+        int yPos = (int)(y0 + yCalc);
+        int xNeg = (int)(x0 - xCalc);
+        int yNeg = (int)(y0 - yCalc);
+
+        Location one = new Location(xPos, yPos);
+        Location two = new Location(xNeg, yNeg);
+
+        if (one.distanceSquared(dest) < two.distanceSquared(dest)) {
+            return one;
+        } else {
+            return two;
+        }
+    }
+
+    /**
+     * computeAStar uses the AStar algorithm to generate the shortest feasible
+     * path to a location.
+     * @param dest The destination the unit is expected to travel to.
+     * @return A list of waypoints.
+     */
+    public ArrayList<Waypoint> computeAStar(Location dest) {
+        Heap openList = new Heap(2500);
+        HashSet<Waypoint> closedList = new HashSet<>();
+        ArrayList<Waypoint> waypoints = new ArrayList<>();
 
         Waypoint start = new Waypoint(uc.getLocation());
         openList.add(start);
 
-        while (!openList.isEmpty()) {
-            Waypoint currentWaypoint = openList.get(0);
-            for (Waypoint w : openList) {
-                if (w.getFCost() < currentWaypoint.getFCost() || w.getFCost() == currentWaypoint.getFCost() && w.hCost < currentWaypoint.hCost) {
-                    currentWaypoint = w;
-                }
-            }
-
-            openList.remove(currentWaypoint);
+        while (openList.count() > 0) {
+            Waypoint currentWaypoint = openList.removeFirst();
             closedList.add(currentWaypoint);
 
             if (currentWaypoint.loc.isEqual(dest)) {
@@ -111,23 +169,24 @@ public abstract class MyUnit {
                     waypoints.add(temp);
                     temp = temp.parent;
                 }
+                Collections.reverse(waypoints);
 
                 return waypoints;
             }
 
             for (Direction dir : dirs) {
                 Waypoint neighbor = new Waypoint(currentWaypoint.loc.add(dir));
-                if (!uc.canSenseLocation(neighbor.loc) || !uc.isAccessible(neighbor.loc) || closedList.contains(neighbor) || uc.senseUnitAtLocation(neighbor.loc) != null) {
+                if (!uc.canSenseLocation(neighbor.loc) || !uc.isAccessible(neighbor.loc) || inClosedList(neighbor, closedList) || uc.senseUnitAtLocation(neighbor.loc) != null) {
                     continue;
                 }
 
                 int movCost = currentWaypoint.gCost + currentWaypoint.loc.distanceSquared(neighbor.loc);
-                if (movCost < neighbor.gCost || !openList.contains(neighbor)) {
+                if (movCost < neighbor.gCost || !openList.heapContains(neighbor)) {
                     neighbor.gCost = movCost;
                     neighbor.hCost = neighbor.loc.distanceSquared(dest);
                     neighbor.parent = currentWaypoint;
 
-                    if (!openList.contains(neighbor)) {
+                    if (!openList.heapContains(neighbor)) {
                         openList.add(neighbor);
                     }
                 }
@@ -137,41 +196,62 @@ public abstract class MyUnit {
         return waypoints;
     }
 
-    public boolean goToLocation(Location dest) {
-        int retries = 3;
+    /**
+     * getPath generates waypoints for the unit to follow. Currently,
+     * only AStar is used - but support for different algorithms will be added.
+     * NOTE: if the unit can't see the destination, getPath will pass a location
+     * closest to the destination and within the detection range (using algebra).
+     * @param dest The destination the unit is expected to travel to.
+     * @return A list of waypoints.
+     */
+    public ArrayList<Waypoint> getPath(Location dest) {
+        Location estimate;
 
-        while(!uc.getLocation().isEqual(dest) && retries > 0) {
-            retries--;
+        estimate = dest;
 
-            if (!uc.canSenseLocation(dest)) {
-                Location[] localArea = uc.getVisibleLocations();
+        if (!uc.canSenseLocation(dest)) {
+            estimate = computeEstimate(dest);
 
-                Location closest = null;
-                for (Location loc : localArea) {
-                    if (closest == null) {
-                        closest = loc;
-                    }
-
-                    if (loc.distanceSquared(dest) < closest.distanceSquared(dest) && uc.isAccessible(loc)) {
-                        closest = loc;
+            if (!uc.isAccessible(estimate)) {
+                for (Direction dir : dirs) {
+                    Location neighbor = estimate.add(dir);
+                    if (uc.canSenseLocation(neighbor) && uc.isAccessible(neighbor)) {
+                        estimate = neighbor;
+                        break;
                     }
                 }
-
-                dest = closest;
             }
-
-            PriorityQueue<Waypoint> path = ComputePath(dest);
-
-            for (Waypoint w : path) {
-                if (uc.canMove()) {
-                    uc.move(uc.getLocation().directionTo(w.loc));
-                }
-            }
-
-            System.out.println(uc.getEnergyLeft());
         }
 
-        return retries > 0;
+        return computeAStar(estimate);
+    }
+
+    /**
+     * goToLocation controls the movement objective of the unit.
+     * It updates the target destination and provides an initial path.
+     * @param dest This is the destination the unit is expected to travel to.
+     */
+    public void goToLocation(Location dest) {
+        if (!dest.isEqual(currentDestination)) {
+            currentDestination = dest;
+            currentPath = getPath(dest);
+        }
+    }
+
+    /**
+     * FollowPath should be called every round. It moves the unit
+     * along the current path in storage. If the end is reached and
+     * the unit isn't at the target destination, a new path will be generated.
+     */
+    public void FollowPath() {
+        if (uc.canMove() && !currentPath.isEmpty()) {
+            uc.move(uc.getLocation().directionTo(currentPath.get(0).loc));
+            currentPath.remove(0);
+
+            if (currentPath.isEmpty() && !uc.getLocation().isEqual(currentDestination)) {
+                currentPath = getPath(currentDestination);
+            }
+        }
     }
 }
 
